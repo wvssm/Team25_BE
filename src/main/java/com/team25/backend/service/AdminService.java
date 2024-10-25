b192548f962fd8a7975ad6f631e55045e6c21d31
wvssm <neoramiregi@gmail.com>
2024-10-25 23:33:50 +0900

    feat(AdminService): 사용자의 Role을 변경하는 로직 추가

--- src/main/java/com/team25/backend/service/AdminService.java
+++ src/main/java/com/team25/backend/service/AdminService.java
@@ -2,11 +2,13 @@ package com.team25.backend.service;
 
 import com.team25.backend.dto.response.AdminPageResponse;
 import com.team25.backend.entity.Manager;
+import com.team25.backend.entity.User;
 import com.team25.backend.repository.UserRepository;
 import com.team25.backend.entity.Certificate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -35,5 +37,13 @@ public class AdminService {
                 })
                 .collect(Collectors.toList());
     }
-}
 
+    public void changeUserRole(Long userId, String role) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            user.updateRole(role);
+            userRepository.save(user);
+        }
+    }
+}
