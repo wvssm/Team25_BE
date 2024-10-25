71e067b70cecaf8d191fb6500c1ec6cba63d4d63
wvssm <neoramiregi@gmail.com>
2024-10-25 23:29:04 +0900

    feat(AdminService): Manager가 존재하는 User 조회 서비스 로직 생성

---
+++ src/main/java/com/team25/backend/service/AdminService.java
@@ -0,0 +1,39 @@
+package com.team25.backend.service;
+
+import com.team25.backend.dto.response.AdminPageResponse;
+import com.team25.backend.entity.Manager;
+import com.team25.backend.repository.UserRepository;
+import com.team25.backend.entity.Certificate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class AdminService {
+    private final UserRepository userRepository;
+
+    public AdminService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<AdminPageResponse> getAllUsersWithManagers() {
+        return userRepository.findUsersWithManager().stream()
+                .map(user -> {
+                    Manager manager = user.getManager();
+                    List<String> certificateImages = manager.getCertificates().stream()
+                            .map(Certificate::getCertificateImage)
+                            .collect(Collectors.toList());
+
+                    return new AdminPageResponse(
+                            user.getId(),
+                            user.getUsername(),
+                            user.getRole(),
+                            manager.getManagerName(),
+                            certificateImages
+                    );
+                })
+                .collect(Collectors.toList());
+    }
+}
+
