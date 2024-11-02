750760e8759aa6f83e9a497d9d6d622050661b8c
Kim Sanghae <pelikan@pusan.ac.kr>
2024-11-02 22:22:36 +0900

    feat(AccompanyService): 예약 ID를 통한 실시간 동행 위치 정보 조회 추가

--- src/main/java/com/team25/backend/service/AccompanyService.java
+++ src/main/java/com/team25/backend/service/AccompanyService.java
@@ -1,6 +1,7 @@
 package com.team25.backend.service;
 
 import com.team25.backend.dto.request.AccompanyRequest;
+import com.team25.backend.dto.response.AccompanyCoordinateResponse;
 import com.team25.backend.dto.response.AccompanyResponse;
 import com.team25.backend.entity.Accompany;
 import com.team25.backend.entity.Reservation;
@@ -30,50 +31,64 @@ public class AccompanyService {
     }
 
     public List<AccompanyResponse> getTrackingAccompanies(Long reservationId) {
-        if(reservationRepository.findById(reservationId).isEmpty()) {
-            throw new ReservationException(ReservationErrorCode.RESERVATION_NOT_FOUND);
-        }
+        checkReservationNull(reservationId);
         List<Accompany> accompanies = accompanyRepository.findByReservation_id(reservationId);
-        if(accompanies.isEmpty()) {
-            throw new ReservationException(ReservationErrorCode.RESERVATION_WITHOUT_ACCOMPANY);
-        }
-
-        return accompanies.stream()
-            .map(accompany -> new AccompanyResponse(
-                accompany.getAccompanyStatus(),
-                accompany.getLatitude(),
-                accompany.getLongitude(),
-                accompany.getTime(),
-                accompany.getDetail()
-            ))
-            .peek(response -> log.info("Accompany details: {}", response))
-            .toList();
+        checkListEmpty(accompanies);
+        return accompanies.stream().map(AccompanyService::getAccompanyResponse)
+            .peek(response -> log.info("Accompany details: {}", response)).toList();
     }
 
-    public AccompanyResponse addTrackingAccompany(Long reservationId, AccompanyRequest accompanyRequest) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime accompanyDateTime = LocalDateTime.parse(accompanyRequest.statusDate(), formatter);
+    public List<AccompanyCoordinateResponse> getTrackingCoordinates(Long reservationId) {
+        checkReservationNull(reservationId);
+        List<Accompany> searchedAccompanies = accompanyRepository.findByReservation_id(reservationId);
+        checkListEmpty(searchedAccompanies);
+        return searchedAccompanies.stream().map(AccompanyService::getAccompanyCoordinateResponse)
+            .peek(reseponse -> log.info("Accompany details: {}", reseponse)).toList();
+    }
 
-        Reservation reservation = reservationRepository.findById(reservationId)
-            .orElseThrow(() -> new ReservationException(ReservationErrorCode.RESERVATION_NOT_FOUND));
-
-        Accompany track = Accompany.builder()
-            .accompanyStatus(accompanyRequest.status())
-            .time(accompanyDateTime)
-            .latitude(accompanyRequest.latitude())
-            .longitude(accompanyRequest.longitude())
-            .detail(accompanyRequest.statusDescribe())
-            .build();
-
+    public AccompanyResponse addTrackingAccompany(Long reservationId,
+        AccompanyRequest accompanyRequest) {
+        LocalDateTime accompanyDateTime = getLocalDateTime(accompanyRequest);
+        Reservation reservation = reservationRepository.findById(reservationId).orElseThrow(
+            () -> new ReservationException(ReservationErrorCode.RESERVATION_NOT_FOUND));
+        Accompany track = getAccompany(accompanyRequest, accompanyDateTime);
         accompanyRepository.save(track);
         reservation.addAccompany(track);
         reservationRepository.save(reservation);
+        return getAccompanyResponse(track);
+    }
 
-        return new AccompanyResponse(
-            track.getAccompanyStatus(),
-            track.getLatitude(),
-            track.getLongitude(),
-            track.getTime(),
+    private static LocalDateTime getLocalDateTime(AccompanyRequest accompanyRequest) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return LocalDateTime.parse(accompanyRequest.statusDate(), formatter);
+    }
+
+    private static AccompanyResponse getAccompanyResponse(Accompany track) {
+        return new AccompanyResponse(track.getAccompanyStatus(), track.getTime(),
             track.getDetail());
+    }
+
+    private static AccompanyCoordinateResponse getAccompanyCoordinateResponse(Accompany track) {
+        return new AccompanyCoordinateResponse(track.getLatitude(), track.getLongitude());
+    }
+
+    private static Accompany getAccompany(AccompanyRequest accompanyRequest,
+        LocalDateTime accompanyDateTime) {
+        return Accompany.builder().accompanyStatus(accompanyRequest.status())
+            .time(accompanyDateTime).latitude(accompanyRequest.latitude())
+            .longitude(accompanyRequest.longitude()).detail(accompanyRequest.statusDescribe())
+            .build();
+    }
+
+    private void checkReservationNull(Long reservationId) {
+        if (reservationRepository.findById(reservationId).isEmpty()) {
+            throw new ReservationException(ReservationErrorCode.RESERVATION_NOT_FOUND);
+        }
+    }
+
+    private static void checkListEmpty(List<Accompany> searchedAccompanies) {
+        if (searchedAccompanies.isEmpty()) {
+            throw new ReservationException(ReservationErrorCode.RESERVATION_WITHOUT_ACCOMPANY);
+        }
     }
 }
