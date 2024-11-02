5e98feb1d49410898bc7389c541558e6af87d9bc
wvssm <neoramiregi@gmail.com>
2024-11-03 00:59:18 +0900

    feat(ManagerRepository): findByUserId 메서드 추가

--- src/main/java/com/team25/backend/repository/ManagerRepository.java
+++ src/main/java/com/team25/backend/repository/ManagerRepository.java
@@ -4,7 +4,9 @@ import com.team25.backend.entity.Manager;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ManagerRepository extends JpaRepository<Manager, Long> {
   List<Manager> findByWorkingRegion(String workingRegion);
+  Optional<Manager> findByUserId(Long userId);
 }
