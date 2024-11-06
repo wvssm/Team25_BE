074b914f4c8f10a7c8095bc1ac4062c147e6aaaf
Kim Sanghae <pelikan@pusan.ac.kr>
2024-11-06 15:16:53 +0900

    feat(ReservationStatus): 예약 상황 진행중, 완료 추가

--- src/main/java/com/team25/backend/enumdomain/ReservationStatus.java
+++ src/main/java/com/team25/backend/enumdomain/ReservationStatus.java
@@ -1,16 +1,18 @@
 package com.team25.backend.enumdomain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 public enum ReservationStatus {
     @JsonProperty("보류")
     HOLD,
 
+    @JsonProperty("취소")
+    CANCEL,
+
     @JsonProperty("확정")
     CONFIRMED,
 
-    @JsonProperty("취소")
-    CANCEL,
+    @JsonProperty("진행중")
+    IN_PROGRESS,
 
     @JsonProperty("완료")
     COMPLETED
