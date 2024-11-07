8e82fc16c2852300c978cf37815c0d3e58ff4b44
unknown <p.plue1881@gmail.com>
2024-11-07 14:02:17 +0900

    feat: bid 응답에 포함되지 않도록 수정

--- src/main/java/com/team25/backend/dto/response/ExpireBillingKeyResponse.java
+++ src/main/java/com/team25/backend/dto/response/ExpireBillingKeyResponse.java
@@ -1,10 +1,13 @@
 package com.team25.backend.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record ExpireBillingKeyResponse(
         String resultCode,
         String resultMsg,
         String tid,
         String orderId,
+        @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
         String bid,
         String authDate
 ) {}
