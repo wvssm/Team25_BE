15bb78e280b04d0336548eb28d0a844e42b865fe
wvssm <neoramiregi@gmail.com>
2024-11-13 12:23:32 +0900

    chore: 사용 하지 않는 클래스 삭제

--- src/main/java/com/team25/backend/domain/user/dto/response/UserRoleStatusResponse.java
+++
@@ -1,8 +0,0 @@
-package com.team25.backend.domain.user.dto.response;
-
-import com.team25.backend.domain.user.enumdomain.UserStatus;
-
-public record UserRoleStatusResponse(
-    UserStatus userStatus
-) {
-}
