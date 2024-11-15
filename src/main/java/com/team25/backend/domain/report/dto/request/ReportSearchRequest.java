b2f49456de24c739864041d3a0b198d40d8062ec
Kim Sanghae <pelikan@pusan.ac.kr>
2024-11-15 12:10:49 +0900

    chore($project): 미사용 파일 정리

--- src/main/java/com/team25/backend/domain/report/dto/request/ReportSearchRequest.java
+++
@@ -1,8 +0,0 @@
-package com.team25.backend.domain.report.dto.request;
-
-import com.team25.backend.domain.report.enumdomain.MedicineTime;
-
-public record ReportSearchRequest(
-    MedicineTime medicineTime
-    ) {
-}
