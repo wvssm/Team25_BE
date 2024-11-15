070d00333fa5f69c7b5775f8b1b353230dd70bad
unknown <p.plue1881@gmail.com>
2024-11-15 12:48:10 +0900

    feat: 예외 처리 통합

--- src/main/java/com/team25/backend/global/exception/ErrorCode.java
+++ src/main/java/com/team25/backend/global/exception/ErrorCode.java
@@ -59,7 +59,17 @@ public enum ErrorCode {
     INVALID_FORMAT_TOKEN(HttpStatus.BAD_REQUEST, "잘못된 형식의 JWT 토큰입니다."),
     NOT_REFRESH_TOKEN(HttpStatus.BAD_REQUEST, "해당 JWT 토큰은 Refresh 토큰이 아닙니다."),
     NOT_EXISTED_REFRESH_TOKEN(HttpStatus.BAD_REQUEST,"해당 Refresh 토큰이 존재하지 않습니다."),
-    FAIL_LOGIN(HttpStatus.UNAUTHORIZED, "로그인을 실패했습니다.");
+    FAIL_LOGIN(HttpStatus.UNAUTHORIZED, "로그인을 실패했습니다."),
+
+    // 결제 에러 코드
+    BILLING_KEY_NOT_FOUND(HttpStatus.NOT_FOUND, "빌링키를 찾을 수 없습니다."),
+    BILLING_KEY_EXISTS(HttpStatus.CONFLICT, "이미 빌링키가 존재합니다."),
+    PAYMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "결제를 찾을 수 없습니다."),
+    PAYMENT_USER_MISMATCH(HttpStatus.FORBIDDEN, "결제 사용자와 요청 사용자가 일치하지 않습니다."),
+    FAILED_TO_REQUEST_BILLING_KEY(HttpStatus.INTERNAL_SERVER_ERROR, "빌링키 요청에 실패했습니다."),
+    FAILED_TO_PROCESS_PAYMENT(HttpStatus.INTERNAL_SERVER_ERROR, "결제 처리에 실패했습니다."),
+    FAILED_TO_EXPIRE_BILLING_KEY(HttpStatus.INTERNAL_SERVER_ERROR, "빌링키 삭제에 실패했습니다."),
+    ;
 
     private final HttpStatus status;
     private final String message;
