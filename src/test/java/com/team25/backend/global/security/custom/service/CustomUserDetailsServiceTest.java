dd37e85e3034f90298abe299f23f4285a07f7009
wvssm <neoramiregi@gmail.com>
2024-11-15 11:36:24 +0900

    test: CustomUserDetailsService 테스트 작성

---
+++ src/test/java/com/team25/backend/global/security/custom/service/CustomUserDetailsServiceTest.java
@@ -0,0 +1,59 @@
+package com.team25.backend.global.security.custom.service;
+
+import com.team25.backend.domain.user.entity.User;
+import com.team25.backend.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class CustomUserDetailsServiceTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private CustomUserDetailsService customUserDetailsService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("사용자가 존재하면 UserDetails를 반환한다")
+    void loadUserByUsername_사용자가_존재하는_경우() {
+        // given
+        String username = "testUser";
+        User expectedUser = new User("username","ROLE_USER","user-uuid");
+        when(userRepository.findByUsername(username)).thenReturn(Optional.of(expectedUser));
+
+        // when
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+
+        // then
+        assertThat(userDetails.getUsername()).isEqualTo(expectedUser.getUsername());
+        verify(userRepository, times(1)).findByUsername(username);
+    }
+
+    @Test
+    @DisplayName("사용자가 존재하지 않으면 예외가 발생한다")
+    void loadUserByUsername_사용자가_존재하지_않는_경우() {
+        // given
+        String username = "nonExistentUser";
+        when(userRepository.findByUsername(username)).thenReturn(Optional.empty());
+
+        // when & then
+        assertThrows(UsernameNotFoundException.class, () -> customUserDetailsService.loadUserByUsername(username));
+        verify(userRepository, times(1)).findByUsername(username);
+    }
+}
